a5b0d7026d4667c77185a493ff9dfbf6b290fa7d
Ygor Santos <santosygor2007@gmail.com>
2026-09-08 20:32:28 -0300

    chore: update user status on the schema

---
+++ prisma/migrations/20260908231231_update_user_status/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - The values [SUSPENDED] on the enum `UserStatus` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "UserStatus_new" AS ENUM ('ACTIVE', 'INACTIVE', 'CANCELED');
+ALTER TABLE "public"."User" ALTER COLUMN "status" DROP DEFAULT;
+ALTER TABLE "User" ALTER COLUMN "status" TYPE "UserStatus_new" USING ("status"::text::"UserStatus_new");
+ALTER TYPE "UserStatus" RENAME TO "UserStatus_old";
+ALTER TYPE "UserStatus_new" RENAME TO "UserStatus";
+DROP TYPE "public"."UserStatus_old";
+ALTER TABLE "User" ALTER COLUMN "status" SET DEFAULT 'ACTIVE';
+COMMIT;
